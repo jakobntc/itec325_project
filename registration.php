dc82885c2b85715773e652840ace457bd9386fca
jakobntc <jclapsaddle@radford.edu>
2021-03-30 01:07:31 -0400

    Added the footer to the page along with the corresponding style in the css file.

--- registration.php
+++ registration.php
@@ -105,6 +105,13 @@ require_once("utils/constants.php");
     </div>
     <input class="btn btn-primary" type="submit"></button>
 </form>
+
+<footer class="container-fluid">
+    <hr class="featurette-divider" id="footerDivider" style="width: 100%;">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2021-2022, inc. &middot; <a href="#">Privacy</a> &middot; <a              href="#">Terms</a></p>
+</footer>
+
 </body>
 
 </html>
