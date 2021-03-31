f10a144970e00f92742d785c1fe54a6915d8ca04
jakobntc <jclapsaddle@radford.edu>
2021-03-31 09:21:13 -0400

    Added the initial login page.

---
+++ login.php
@@ -0,0 +1,78 @@
+<?php
+    require_once("utils/utils.php");
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Our website registration</title>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel='stylesheet' href='styles/reg_style.css'>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+<!-- Navbar for the page. -->
+<header>
+    <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Team Alpha Website</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-                       controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="homepage.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Repair Tickets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Post a Room</a>
+                    </li>
+                    <li class="nav-item">
+                        <form method="get" action="#">
+                            <input type="text" id="searchText" name="searchText" placeholder="Search...">
+                            </input>
+                            <button class="btn btn-primary" type="submit">Search</button>
+                        </form>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class=nav-item">
+                        <form method="get" action="#">
+                            <button class="btn btn-primary" type="submit">Login</button>
+                        </form>
+                    </li>
+                    <li class=nav-item">
+                        <form method="get" action="registration.php">
+                            <button class="btn btn-primary" type="submit">Sign Up</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<!-- The footer of the page. -->
+<footer class="container-fluid">
+    <hr class="featurette-divider" id="footerDivider" style="width: 100%;">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2021-2022, inc. &middot; <a href="#">Privacy</a> &middot; <a                  href="#">Terms</a></p>
+</footer>
+
+</body>
+</html>
