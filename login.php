cf6e54c14811c89815792803bc0a9ecf3d996686
jakobntc <jclapsaddle@radford.edu>
2021-03-31 14:54:40 -0400

    Finished the login page

--- login.php
+++ login.php
@@ -8,17 +8,14 @@
 <html>
 
 <head>
-    <title>Our website registration</title>
+    <title>Our website login</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel='stylesheet' href='styles/reg_style.css'>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel='stylesheet' href='styles/login_style.css'>
 </head>
 
 <body>
@@ -27,7 +24,7 @@
 <header>
     <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
         <div class="container-fluid">
-            <a class="navbar-brand" href="homepage.php">Team Alpha Website</a>
+            <a class="navbar-brand" href="homepage.php"><img src="photos/logo.webp" width="30" height="30">Team Alpha Website</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-                       controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -67,12 +64,31 @@
     </nav>
 </header>
 
-<!-- The footer of the page. -->
-<footer class="container-fluid">
-    <hr class="featurette-divider" id="footerDivider" style="width: 100%;">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2021-2022, inc. &middot; <a href="#">Privacy</a> &middot; <a                  href="#">Terms</a></p>
-</footer>
+<main class="form-signin">
+    <div class="shadow-lg border rounded rounded-3" style="height: 400px; padding: 10px;">
+        <form style="margin: 5px;">
+            <img class="mb-4" src="photos/logo.webp" alt="" width="57" height="57">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating" style="margin: 3px;">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Username" required minlength="10" maxlength="25">
+            </div>
+            <div class="form-floating" style="margin: 3px;">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required minlength="10" maxlength="25">
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted"><a href="#">Terms</a> & <a href="#">conditions</a></p>
+        </form>
+    </div>
+</main>
 
 </body>
+
+<!--
+    <footer class="container">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2017-2019, inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
+-->
+
 </html>
