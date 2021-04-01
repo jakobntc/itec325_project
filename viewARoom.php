f381a7b315261752b20bfe1c297627b9b4d9b289
jakobntc <jclapsaddle@radford.edu>
2021-03-31 23:46:31 -0400

    Added the navbar/footer/carousel to the viewARoom page along with the style.

---
+++ viewARoom.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+ <head>
+     <title>Our website homepage</title>
+     <meta charset='utf-8'>
+     <meta name='viewport' content='width=device-width, initial-scale=1'>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+     <link rel='stylesheet' href='styles/style.css'>
+ </head>
+
+<body>
+
+<header>
+    <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Team Alpha Website</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="homepage.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Repair Tickets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Post a Room</a>
+                    </li>
+                    <li class="nav-item">
+                        <form method="get" action="#">
+                            <input type="text" id="searchText" name="searchText" placeholder="Search...">
+                            </input>
+                            <button class="btn btn-primary" type="submit">Search</button>
+                        </form>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class=nav-item">
+                        <form method="get" action="login.php">
+                            <button class="btn btn-primary" type="submit">Login</button>
+                        </form>
+                    </li>
+                    <li class=nav-item">
+                        <form method="get" action="registration.php">
+                            <button class="btn btn-primary" type="submit">Sign Up</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
+<div id="carouselContainer" class="container">
+
+    <!-- carousel -->
+    <div id="roomPhotos" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Items in the carousel. -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="photos/fakeBedRoomPicture.png" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="photos/fakeBedRoomPicture_2.png" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="photos/fakeBedRoomPicture_3.png" class="d-block w-100">
+            </div>
+        </div>
+
+        <!-- Buttons for the controls. -->
+        <a class="carousel-control-prev" role="button" href="#roomPhotos" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" role="button" href="#roomPhotos" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </a>
+    </div> 
+    <!-- /carousel -->
+
+</div>
+
+<!-- Footer for the page. -->
+<footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2017-2019, inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+
+</body>
+
+</html>
