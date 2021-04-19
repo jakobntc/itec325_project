95931a1855d1f40b3f65f45c6b27241efec08e95
jakobntc <jclapsaddle@radford.edu>
2021-04-19 11:43:21 -0400

    Added the database connectiong function.

--- database-connection.php
+++ database-connection.php
@@ -1,16 +1,18 @@
 <?php
 
-$host = "localhost";
-$username = "jclapsaddle";
-$password = "mysqlpassword123";
-$dbname = "jclapsaddle";
+function connectToDatabase() {
+    $host = "itec-php01";
+    $username = "325proj2";
+    $password = "325proj2!";
+    $dbname = "325proj2";
 
-$con = mysqli_connect($host, $username, $password, $dbname);
+    $con = mysqli_connect($host, $username, $password, $dbname);
 
-if (!$con) {
-    echo "Database connection failed.";
-} else {
-    echo "Database connection succeeded.";
+    if (!$con) {
+        echo "Database connection failed.\n";
+    } else {
+        return $con;
+    }
 }
 
 ?>
