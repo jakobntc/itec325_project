b668a1f4089b92d79094cffd3e34ebc5cd67bd6d
jakobntc <jclapsaddle@radford.edu>
2021-04-19 23:42:59 -0400

    Adding the login handler page.

---
+++ login-handler.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("database-connection.php");
+
+$con = connectToDatabase();
+
+$username = mysqli_real_escape_string($con, $_POST["username"]);
+$password = hash("sha256", mysqli_real_escape_string($con, $_POST["password"]));
+
+$query = "SELECT * FROM Users
+          WHERE User_Name = '$username'
+          AND Password = '$password'";
+$allRows = mysqli_query($con, $query);
+if (!$allRows) echo "Query failed -- connection lost\n?";
+
+$oneRow = mysqli_fetch_array($allRows);
+
+// The return table didn't contain any rows
+if (!$oneRow) {
+    echo "Invalid username or password.\n";
+} else {
+    echo "Valid username & password provided.\n";
+}
+
+$
+
+?>
