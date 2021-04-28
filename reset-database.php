c19ece585d8f5a55bc3eb7de46e920edc2d54532
Jakob Clapsaddle <jclapsaddle@rucs.radford.edu>
2021-04-28 02:16:24 -0400

    Added the city/ state to the Rooms table.

--- reset-database.php
+++ reset-database.php
@@ -34,12 +34,14 @@ $dropAndCreateTables = array( "dropReservations" => "DROP TABLE IF EXISTS Reserv
                                                                                  )"
                             , "createRooms" => "CREATE TABLE Rooms ( Room_ID INTEGER AUTO_INCREMENT
                                                                    , User_ID INTEGER NOT NULL
-                                                                   , Title VARCHAR(20) NOT NULL
-                                                                   , Description VARCHAR(200) NOT NULL
+                                                                   , Title VARCHAR(124) NOT NULL
+                                                                   , Description VARCHAR(1024) NOT NULL
                                                                    , Price_Per_Night DOUBLE NOT NULL
                                                                    , Number_of_Bedrooms INTEGER NOT NULL
                                                                    , Number_of_Bathrooms INTEGER NOT NULL
                                                                    , Square_footage INTEGER NOT NULL
+								                                   , City VARCHAR(60) NOT NULL
+								                                   , State VARCHAR(60) NOT NULL
                                                                    , CONSTRAINT PK_Rooms PRIMARY KEY (Room_ID)
                                                                    , CONSTRAINT FK_Room_Poster FOREIGN KEY (User_ID)
                                                                        REFERENCES Users(User_ID)
@@ -86,7 +88,7 @@ $amenities = array( "Air Conditioning"
 // Loading the amenity data
 //
 foreach ($amenities AS $amenity) {
-    $insert = "INSERT INTO Amenitites (Amen_name)
+    $insert = "INSERT INTO Amenities (Amen_name)
                VALUES ('$amenity')";
     $result = mysqli_query($con, $insert);
     if (!$result) {
@@ -95,6 +97,109 @@ foreach ($amenities AS $amenity) {
         echo "inserted $amenity.\n";
     }
 }
+
+// Loading the default user data
+// 
+$insert = "INSERT INTO Users ( User_Name
+                             , Password
+                             , F_Name
+                             , L_Name
+                             , Email
+                             )
+           VALUES ( 'jakobclapsaddle'
+                  , '"
+                  . hash("sha256", "something1")
+                  . "'
+                  , 'Jakob'
+                  , 'Clapsaddle'
+                  , 'jclapsaddle@radford.edu'
+                  )";
+
+$result = mysqli_query($con, $insert);
+if (!$result) {
+    echo "Something went wrong with the user input.\n";
+    echo "\n $insert \n";
+} else {
+    echo "Everything worked fine!\n";
+}
+
+// Loading the first three rooms into the database to be displayed on the homepage.
+//
+$loadRooms = array( "room1" => "INSERT INTO Rooms ( User_ID
+                                                  , Title
+                                                  , Description
+                                                  , Price_Per_Night
+                                                  , Number_of_Bedrooms
+                                                  , Number_of_Bathrooms
+                                                  , Square_footage
+                                                  , City
+                                                  , State
+                                                  )
+                                VALUES ( 1
+                                       , 'Example Room In Roanoke'
+                                       , 'quite cozy if I do say so myself'
+                                       , 100
+                                       , 2
+                                       , 1
+                                       , 750
+                                       , 'Roanoke'
+                                       , 'Virginia'
+                                       )"
+		  , "room2" => "INSERT INTO Rooms ( User_ID
+                                                  , Title
+                                                  , Description
+                                                  , Price_Per_Night
+                                                  , Number_of_Bedrooms
+                                                  , Number_of_Bathrooms
+                                                  , Square_footage
+                                                  , City
+                                                  , State
+                                                  )
+                                VALUES ( 1
+							 , 'Example Room In Radford'
+							 , 'You could live her next semester'
+							 , 150
+							 , 2
+							 , 2
+							 , 850
+							 , 'Radford'
+							 , 'Virginia'
+							 )"
+		  , "room3" => "INSERT INTO Rooms ( User_ID
+                                          , Title
+                                          , Description
+                                          , Price_Per_Night
+                                          , Number_of_Bedrooms
+                                          , Number_of_Bathrooms
+                                          , Square_footage
+                                          , City
+                                          , State
+                                          )
+                                VALUES ( 1
+							 , 'Example Room In Blacksburg'
+							 , 'This one costs more just because its in Blacksburg'
+							 , 200
+							 , 1
+							 , 1	
+							 , 500
+							 , 'Blacksburg'
+							 , 'Virginia'	
+                             )"
+          );
+
+foreach ($loadRooms AS $insert) {
+
+    $result = mysqli_query($con, $insert);
+    if (!$result) {
+	echo "Something went wrong with the room inserts!<br />";
+    echo "<br /> $insert <br />";
+    } else {
+	echo "Room inserted fine!<br />";
+    }
+}
+
+// Loading the default userdata into the database.
+// 
 
 mysqli_close($con);
 echo "Database connection closed";
